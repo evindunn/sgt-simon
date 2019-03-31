e57acc00a8667b8db1229669351b39bbca37f855
Evin Dunn <evindunn14@gmail.com>
2019-03-30 22:06:30 -0700

    Adds the world

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,26 @@
+let gameVars = new Object();
+
 function preload() {
-  this.load.spritesheet("player", "/img/character.png", { frameWidth: 48, frameHeight: 48 });
+  // Map
+  this.load.spritesheet("groundTiles", "img/groundTiles.png", { frameWidth: 32, frameHeight: 32 });
+  this.load.tilemapTiledJSON("map", "assets/map.json");
+
+  // Player
+  this.load.spritesheet("player", "img/character.png", { frameWidth: 48, frameHeight: 48 });
 }
 
 function create() {
+  gameVars.map = this.make.tilemap({ key: "map" });
+  gameVars.groundTiles = gameVars.map.addTilesetImage("groundTiles");
+  gameVars.groundLayer = gameVars.map.createDynamicLayer(
+    "groundLayer",
+    gameVars.groundTiles
+  );
+  gameVars.groundLayer.y = -gameVars.groundLayer.height + 480;
+  gameVars.groundLayer.setCollisionByExclusion([-1]);
 
+  this.physics.world.bounds.width = gameVars.groundLayer.width;
+  this.physics.world.bounds.height = gameVars.groundLayer.height;
 }
 
 function update() {
@@ -19,6 +36,13 @@ function main() {
       preload: preload,
       create: create,
       update: update
+    },
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 500 },
+        debug: false
+      }
     }
   };
 
